0a35773788f2213d22dca122c5f6a04f11e30d15
SaraToth <saratoth101@gmail.com>
2025-05-21 17:47:15 +0900

    Feat: Create basic routes and controls

---
+++ controllers/indexController.js
@@ -0,0 +1,13 @@
+exports.userListGet = (req, res) => {
+    console.log("usernames will be logged here - wip");
+    res.send("user list get was executed");
+}
+
+exports.userFormGet = (req, res) => {
+    console.log("get a form");
+    res.send("user form get was executed");
+}
+
+exports.userFormPost = (req, res) => {
+    console.log("username to be saved: ");
+}
---
+++ routes/indexRouter.js
@@ -0,0 +1,11 @@
+const { Router } = require("express");
+const indexRouter = Router();
+const indexController = require("../controllers/indexController");
+
+indexRouter.get("/", indexController.userListGet);
+
+indexRouter.get("/new", indexController.userFormGet);
+indexRouter.post("/new", indexController.userFormPost);
+
+
+module.exports = indexRouter;
