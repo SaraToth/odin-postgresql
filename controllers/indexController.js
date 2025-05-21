866b5fdc5168d34f51e3e34fa731216d559fd5fa
SaraToth <saratoth101@gmail.com>
2025-05-21 18:15:44 +0900

    Feat: Update form post and validate input

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,3 +1,11 @@
+const { body, validationResult } = require("express-validator");
+
+const validateUser = [
+    body("username").trim()
+        .isAlphanumeric().withMessage("username must contain only letters and numbers")
+        .isLength({ min: 8, max: 15 }).withMessage("username must be between 8 and 15 characters long"),
+]
+
 exports.userListGet = (req, res) => {
     res.render("index", { title: "Home" });
 }
@@ -6,6 +14,19 @@ exports.userFormGet = (req, res) => {
     res.render("newUser", { title: "Create a New User" });
 }
 
-exports.userFormPost = (req, res) => {
-    console.log("username to be saved: ");
-}
+exports.userFormPost = [
+    validateUser,
+
+    (req, res) => {
+        // const errors = validationResult(req);
+        // if (!errors.isEmpty()) {
+        //     return res.status(400).render("newUser", {
+        //         title: "Create a New User ERROR",
+        //         // errors: errors.array(),
+        //     });
+        // }
+        const { username } = req.body;
+        console.log(username);
+        res.redirect("/");
+    }
+];
