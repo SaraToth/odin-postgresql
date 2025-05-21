12ab0dc38684d11167e7f7a58f1df4a1957858bf
SaraToth <saratoth101@gmail.com>
2025-05-21 19:58:42 +0900

    Feat: Create pool and queries for db

--- db/pool.js
+++ db/pool.js
@@ -1,10 +1,11 @@
 const { Pool } = require("pg");
+require("dotenv").config();
 
 //Comes from environmental variables
 module.exports = new Pool({
-    host: "",
-    user: "",
-    database: "",
-    password: "",
-    port: "",
+    host: process.env.HOST,
+    user: process.env.ROLE_NAME,
+    database: process.env.DATABASE,
+    password: process.env.PASSWORD,
+    port: process.env.PORT,
 });
