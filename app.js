9c7bd4095d5d5bb73bc60947e1b7ec8a9938f5da
SaraToth <saratoth101@gmail.com>
2025-05-21 18:14:43 +0900

    Feat: Set urlencoded to true

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const app = express();
 const path = require("node:path");
-const { body, validationResult } = require("express-validator");
 const indexRouter = require("./routes/indexRouter");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-// app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/", indexRouter);
 
